5d091ce9819e9b8f3efa9ea8d84c2ea6bff0fbef
Loïc <you@example.com>
2020-09-28 15:02:18 +0200

    TP Back Collegue API ok

--- src/main/java/dev/services/CollegueService.java
+++ src/main/java/dev/services/CollegueService.java
@@ -1,153 +1,51 @@
 package dev.services;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import dev.dto.CreerCollegueRequestDto;
 import dev.entite.Collegue;
-import dev.repository.CollegueRepository;
+import dev.repository.CollegueRepo;
 
-/**
- * service gestion des Collegue
- * 
- * @author cql-v2
- *
- */
 @Service
 public class CollegueService {
 
-	private CollegueRepository colRepo;
+	private CollegueRepo collegueRepo;
 
-	public CollegueService(CollegueRepository colRepo) {
-		this.colRepo = colRepo;
+	public CollegueService(CollegueRepo collegueRepo) {
+		super();
+		this.collegueRepo = collegueRepo;
 	}
 
-	/**
-	 * 
-	 * @return une list d'objet de tout les collegues
-	 */
-	public List<Collegue> getList() {
-		return colRepo.findAll();
+	public List<String> rechercherParNom(String nom) {
+		return collegueRepo.findByNom(nom);
 	}
 
-	/**
-	 * 
-	 * @param id identifiant du collegue recherché
-	 * @return un objet Collegue correspondant au collegue
-	 */
-	public Optional<Collegue> getById(Integer id) {
-		return colRepo.findById(id);
-	}
+	public Collegue creerCollegue(CreerCollegueRequestDto dto) {
 
-	/**
-	 * 
-	 * @param matricule le matricule recherché
-	 * @return un objet Collegue correspondant au matricule
-	 */
-	public Optional<Collegue> getByMatricule(String matricule) {
-		return colRepo.findByMatricule(matricule);
-	}
+		Collegue collegue = new Collegue();
+		collegue.setNom(dto.getNom());
+		collegue.setPrenom(dto.getPrenoms());
+		collegue.setDateNaissance(dto.getDateDeNaissance());
+		collegue.setPhotoUrl(dto.getPhotoUrl());
 
-	/**
-	 * 
-	 * @param nom le nom recherché
-	 * @return une liste d'objet Collegue correspondant au nom
-	 */
-	public List<Collegue> getByNom(String nom) {
-		return colRepo.findAllBynom(nom);
-	}
+		// logique métier (génération du matricule)
+		collegue.setMatricule(UUID.randomUUID().toString());
+		collegue.setEmail(dto.getPrenoms() + "." + dto.getNom() + "@dev.fr");
 
-	/**
-	 * 
-	 * @param nom le nom recherché
-	 * @return une liste de string de tous les matricules correspondant au nom
-	 */
-	public List<String> getAllMatricules() {
-		return colRepo.findMatricule();
-	}
+		// => envoyer un email aux RHs
+		// ...
 
-	/**
-	 * 
-	 * @param id l'identifiant rechercher
-	 * @return un optional contenant le matricule correspondant a l'id
-	 */
-	public Optional<String> getMatriculesById(Integer id) {
-		return colRepo.findMatriculeById(id);
-	}
-
-	/**
-	 * 
-	 * @param nom
-	 * @return un optional contenant le matricule correspondant a un nom
-	 */
-	public Optional<String> getMatriculesByNom(String nom) {
-		return colRepo.findMatriculeByNom(nom);
-	}
-
-	/**
-	 * 
-	 * @param matricule     le matricule du collegue à ajouter
-	 * @param nom           le nom du collegue à ajouter
-	 * @param prenom        le prenom du collegue à ajouter
-	 * @param email         l'email du collegue à ajouter
-	 * @param dateNaissance la date de naissance du collegue à ajouter (format sql)
-	 * @param photoUrl      l'url de l'image du collegue à ajouter
-	 * @return un objet collegue.
-	 */
-	@Transactional
-	public Collegue addCollegue(String nom, String prenom, String email, LocalDate dateNaissance, String photoUrl) {
-
-		return colRepo.save(new Collegue(UUID.randomUUID().toString(), nom, prenom, email, dateNaissance, photoUrl));
-	}
-
-	/**
-	 * 
-	 * @param id            l'id du Collegue
-	 * @param matricule     le matricule du Collegue a metre a jour
-	 * @param nom           le nom du Collegue a metre a jour
-	 * @param prenom        le prenom du Collegue a metre a jour
-	 * @param email         l'email du Collegue a metre a jour
-	 * @param dateNaissance la date de naissance du Collegue a metre a jour
-	 * @param photoUrl      l'url de l'image du Collegue a metre a jour
-	 * @return un objet collegue
-	 */
-	public Collegue updateCollegue(Integer id, String matricule, String nom, String prenom, String email,
-			LocalDate dateNaissance, String photoUrl) {
-		Optional<Collegue> collegueToUpdate = this.getById(id);
-		if (collegueToUpdate.isPresent()) {
-			collegueToUpdate.get().setMatricule(matricule);
-			collegueToUpdate.get().setNom(nom);
-			collegueToUpdate.get().setPrenom(prenom);
-			collegueToUpdate.get().setDateNaissance(dateNaissance);
-			collegueToUpdate.get().setEmail(email);
-			collegueToUpdate.get().setPhotoUrl(photoUrl);
-		}
-		return colRepo.save(collegueToUpdate.get());
+		return this.collegueRepo.save(collegue);// insert into collegue
 
 	}
 
 	public Optional<Collegue> rechercherParMat(String matricule) {
 
-		return this.colRepo.findByMatricule(matricule);
-	}
-
-	/**
-	 * 
-	 * @param id id du collegue a supprimer
-	 * @return un boolean
-	 */
-	public String remUser(Integer id) {
-
-		Optional<Collegue> collegueToRemove = this.getById(id);
-		if (collegueToRemove.isPresent()) {
-			colRepo.delete(collegueToRemove.get());
-			return "Suppression reussi";
-		}
-		return "id non trouvé";
+		return this.collegueRepo.findByMatricule(matricule);
 	}
 
 }
--- src/main/java/dev/web/collegue/CollegueCtrl.java
+++ src/main/java/dev/web/collegue/CollegueCtrl.java
@@ -1,52 +1,47 @@
 package dev.web.collegue;
 
-import static java.util.stream.Collectors.toList;
-
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import dev.dto.CollegueDto;
+import dev.dto.CreerCollegueRequestDto;
+import dev.dto.CreerCollegueResponseDto;
 import dev.entite.Collegue;
 import dev.services.CollegueService;
 
-/**
- * controlleur de l'api Collegue
- * 
- * @version 0.1
- * @author cql-v2
- *
- */
-
+@CrossOrigin
 @RestController
-@RequestMapping("collegues") // ressource "collegues"
+@RequestMapping("collegues")
 public class CollegueCtrl {
 
-	private CollegueService colServ;
+	private CollegueService collegueService;
 
-	public CollegueCtrl(CollegueService colServ) {
-		this.colServ = colServ;
+	public CollegueCtrl(CollegueService collegueService) {
+		super();
+		this.collegueService = collegueService;
 	}
 
+	// GET /collegues
 	@GetMapping
-	public List<String> listCollegue(@RequestParam String nom) {
-		return colServ.getByNom(nom).stream().map(Collegue::getMatricule).collect(toList());
-
+	public List<String> rechercherMatriculesParNom(@RequestParam String nom) {
+		return this.collegueService.rechercherParNom(nom);
 	}
 
+	// GET /collegues/????
 	@GetMapping("{matricule}")
 	public ResponseEntity<?> rechercherParMatricule(@PathVariable String matricule) {
-		Optional<Collegue> optCol = this.colServ.rechercherParMat(matricule);
+		Optional<Collegue> optCol = this.collegueService.rechercherParMat(matricule);
 
 		if (optCol.isPresent()) {
 			return ResponseEntity.ok(optCol.get());
@@ -56,58 +51,12 @@ public class CollegueCtrl {
 
 	}
 
-	/**
-	 * url : [SERVER]/collegue/getMatricule?nom={nom}
-	 * 
-	 * @return un json de tous les matricules en bdd
-	 */
-	@GetMapping("{matricule}")
-	public ResponseEntity<?> findByMatricule(@PathVariable String matricule) {
-		Optional<Collegue> byMatricule = colServ.getByMatricule(matricule);
-
-		if (byMatricule.isPresent()) {
-			return ResponseEntity.ok(byMatricule.get());
-		} else {
-			return ResponseEntity.notFound().build();
-		}
-	}
-
-	/**
-	 * url : [SERVER]/collegue
-	 * 
-	 * @param collegueDto un objet collegueDto au format json
-	 * @return un objet collegueDto au format json
-	 */
+	// POST /collegues
 	@PostMapping
-	public ResponseEntity<?> newCollegue(@RequestBody CollegueDto collegueDto) {
-		Collegue newCollegue = colServ.addCollegue(collegueDto.getNom(), collegueDto.getPrenom(),
-				collegueDto.getEmail(), collegueDto.getDateNaissance(), collegueDto.getPhotoUrl());
+	public CreerCollegueResponseDto creerNouveauCollegue(@RequestBody @Valid CreerCollegueRequestDto dto) {
+		Collegue collegueCree = this.collegueService.creerCollegue(dto);
 
-		return ResponseEntity.ok(newCollegue);
-	}
-
-	/**
-	 * 
-	 * @param collegueDto un objet collegueDto au format json
-	 * @return un objet collegueDto au format json
-	 */
-	@PutMapping
-	public ResponseEntity<?> editUser(@RequestBody CollegueDto collegueDto) {
-		Collegue editCollegue = colServ.updateCollegue(collegueDto.getId(), collegueDto.getMatricule(),
-				collegueDto.getNom(), collegueDto.getPrenom(), collegueDto.getEmail(), collegueDto.getDateNaissance(),
-				collegueDto.getPhotoUrl());
-		return ResponseEntity.ok(editCollegue);
-	}
-
-	/**
-	 * 
-	 * @param id id du collegue a remove
-	 * @return une String
-	 */
-	@DeleteMapping
-	public ResponseEntity<?> remUser(@RequestParam Integer id) {
-		return ResponseEntity.ok(colServ.remUser(id));
-
+		return new CreerCollegueResponseDto(collegueCree.getMatricule(), collegueCree.getEmail());
 	}
 
 }
